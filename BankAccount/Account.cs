19b242ef23b43d05706b00bd2af0c28691a4da37
Mirza Mohammad Azwad <50703605+mirzaazwad@users.noreply.github.com>
2021-12-08 00:10:06 +0600

    Add files via upload

---
+++ BankAccount/Account.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Account
+{
+	private string Number;
+	private double Balance;
+	private string Branch;
+	public Account(string Number, double Balance, string Branch)
+	{
+		this.Number = Number;
+		this.Balance = Balance;
+		this.Branch = Branch;
+	}
+	public Account()
+    {
+
+    }
+	public string GetNumber()
+    {
+		return this.Number;
+    }
+	public void SetAccount(string Number, double Balance, string Branch)
+    {
+		this.Number = Number;
+		this.Balance = Balance;
+		this.Branch = Branch;
+	}
+	public void MakeWithdrawl(double withdraw)
+    {
+		this.Balance-=withdraw;
+    }
+	public void MakeDeposit(double deposit)
+    {
+		this.Balance+=deposit;
+    }
+	public double ShowBalance()
+    {
+		return this.Balance;
+    }
+}
+
