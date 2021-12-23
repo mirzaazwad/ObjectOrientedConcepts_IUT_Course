9c0c901143ae245ccdf1a79adef441dbb049795c
Mirza Mohammad Azwad <50703605+mirzaazwad@users.noreply.github.com>
2021-12-23 17:06:21 +0600

    Add files via upload

---
+++ LaundryManagementSystem/laundry_management_dependencies/Order.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laundry_management_dependencies
+{
+    public static class Order
+    {
+        public static int OrderID = 0;
+        public static double CurrentAccount=0.00;
+        public static int getOrderID()
+        {
+            return Order.OrderID;
+        }
+        public static void Increment()
+        {
+            Order.OrderID++;
+        }
+        public static void AddToAccount(double money)
+        {
+            Order.CurrentAccount += money;
+        }
+    }
+}
---
+++ LaundryManagementSystem/laundry_management_dependencies/Shirt.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laundry_management_dependencies
+{
+    public class Shirt : Product
+    {
+        public Shirt()
+        {
+
+        }
+        public Shirt(string ToDo, int Quantity)
+        {
+            this.ProductQuantity = Quantity;
+            this.ToDo = ToDo;
+            this.Wash = 7;
+            this.Iron = 5;
+            this.Both = 12;
+            if (ToDo == "Wash")
+            {
+                this.Cost = Quantity * this.Wash;
+            }
+            else if (ToDo == "Iron")
+            {
+                this.Cost= Quantity*this.Iron;
+            }
+            else
+            {
+                this.Cost= Quantity*this.Both;
+            }
+        }
+    }
+}
