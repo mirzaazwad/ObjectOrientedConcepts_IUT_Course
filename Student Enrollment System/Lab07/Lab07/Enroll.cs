8e32fd1202023d2c0f55dc51181bc39b12f9a851
Mirza Mohammad Azwad <50703605+mirzaazwad@users.noreply.github.com>
2022-01-04 00:38:41 +0600

    Add files via upload

---
+++ Student Enrollment System/Lab07/Lab07/Enroll.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lab07_dependecies;
+
+namespace Lab07
+{
+    public static class Enroll
+    {
+        public static List<Student>studentsList=new List<Student>();
+        public static List<Professional>professionalList=new List<Professional>();
+        public static List<Courses>coursesList=new List<Courses>();
+        public static DateTime enrollmentDate = new DateTime();
+        public static List<DateTime>dateList=new List<DateTime>();
+        public static List<string>courseIDList=new List<string>();
+
+    }
+}
