25527d4a8a26418e4a00e05f49687d8c800c189f
Mirza Mohammad Azwad <50703605+mirzaazwad@users.noreply.github.com>
2022-03-01 15:47:49 +0600

    Add files via upload

---
+++ Lab10EmployeeData/WindowsFormsApp1/Utilities.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    public class UtilitiesEmployee
+    {
+        public static List<Employee> employeeList = new List<Employee>();
+    }
+}
